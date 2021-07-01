55be30c2ba4e5a2eedc5d814ac852a887f6ea995
Albert <zuendorf@uni-kassel.de>
2021-07-01 17:19:06 +0200

    Trying project setup (not yet working)

---
+++ src/test/src/gen/org.fulib.workflows/Genmodel.java
@@ -0,0 +1,13 @@
+package org.fulib.workflows;
+
+import org.fulib.builder.ClassModelDecorator;
+import org.fulib.builder.ClassModelManager;
+
+public class GenModel implements ClassModelDecorator
+{
+   @Override
+   public void decorate(ClassModelManager m)
+   {
+      m.
+   }
+}
---
+++ src/test/src/test/java/org/fulib/workflows/TestWorkflowGenerator.java
@@ -0,0 +1,25 @@
+package org.fulib.workflows;
+
+import org.fulib.yaml.Yaml;
+import org.fulib.yaml.Yamler;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+
+public class TestWorkflowGenerator
+{
+   @Test
+   public void testLoadEventModel() throws IOException
+   {
+      String yaml = Files.readString(Path.of("src/test/src/test/resources/org/fulib/workflows/ShopWorkflow.yml"));
+      ArrayList<LinkedHashMap<String, String>> maps = new Yamler().decodeList(yaml);
+
+      System.out.println();
+   }
+}
