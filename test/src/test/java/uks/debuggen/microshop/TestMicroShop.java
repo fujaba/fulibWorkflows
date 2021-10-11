371ff7aea7776267235642b71cd101327089e1c0
Albert <zuendorf@uni-kassel.de>
2021-10-11 19:42:10 +0200

    starting with new test concept

--- test/src/test/java/uks/debuggen/microshop/TestMicroShop.java
+++ test/src/test/java/uks/debuggen/microshop/TestMicroShop.java
@@ -1,4 +1,5 @@
 package uks.debuggen.microshop;
+
 import static com.codeborne.selenide.Condition.matchText;
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.$;
@@ -6,42 +7,51 @@ import static com.codeborne.selenide.Selenide.open;
 
 import java.beans.PropertyChangeSupport;
 import java.util.LinkedHashMap;
+import java.util.Map;
 import java.util.Objects;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.TimeUnit;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import javax.sound.midi.Soundbank;
 
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.SelenideElement;
 import com.mashape.unirest.http.HttpResponse;
 import com.mashape.unirest.http.Unirest;
+import static org.assertj.core.api.Assertions.assertThat;
 
 import org.fulib.yaml.Yaml;
+import org.fulib.yaml.YamlIdMap;
 import org.junit.Before;
 import org.junit.Test;
 
+import spark.Request;
+import spark.Response;
+import spark.Service;
 import uks.debuggen.Constants;
-import uks.debuggen.microshop.Warehouse.WarehouseService;
-import uks.debuggen.microshop.events.DataEvent;
-import uks.debuggen.microshop.events.Event;
-import uks.debuggen.microshop.events.EventBroker;
-import com.mashape.unirest.http.exceptions.UnirestException;
-import uks.debuggen.microshop.events.*;
-import uks.debuggen.microshop.Shop.ShopService;
 import uks.debuggen.microshop.MicroShop.MicroShopService;
+import uks.debuggen.microshop.Warehouse.WarehouseService;
+import uks.debuggen.microshop.events.*;
 
-public class TestMicroShop
-{
+public class TestMicroShop {
    public static final String PROPERTY_EVENT_BROKER = "eventBroker";
    private EventBroker eventBroker;
    protected PropertyChangeSupport listeners;
+   private Service spark;
+   private LinkedBlockingQueue<Event> eventQueue;
+   private LinkedHashMap<String, Event> history = new LinkedHashMap<>();
+   private int port = 41999;
 
-   public EventBroker getEventBroker()
-   {
+   public EventBroker getEventBroker() {
       return this.eventBroker;
    }
 
-   public TestMicroShop setEventBroker(EventBroker value)
-   {
-      if (Objects.equals(value, this.eventBroker))
-      {
+   public TestMicroShop setEventBroker(EventBroker value) {
+      if (Objects.equals(value, this.eventBroker)) {
          return this;
       }
 
@@ -51,17 +61,134 @@ public class TestMicroShop
       return this;
    }
 
+
+
+   public void start() {
+      eventQueue = new LinkedBlockingQueue<Event>();
+      ExecutorService executor = Executors.newSingleThreadExecutor();
+      spark = Service.ignite();
+      spark.port(port);
+      spark.post("/apply", (req, res) -> executor.submit(() -> this.postApply(req, res)).get());
+      executor.submit(() -> System.out.println("test executor works"));
+      executor.submit(this::subscribeAndLoadOldEvents);
+      executor.submit(() -> System.out.println("test executor has done subscribeAndLoadOldEvents"));
+      System.out.println();
+   }
+
+   private String postApply(Request req, Response res) {
+      String body = req.body();
+      try {
+         YamlIdMap idMap = new YamlIdMap(Event.class.getPackageName());
+         idMap.decode(body);
+         Map<String, Object> map = idMap.getObjIdMap();
+         for (Object value : map.values()) {
+            Event event = (Event) value;
+            eventQueue.put(event);
+         }
+      } catch (Exception e) {
+         String message = e.getMessage();
+         if (message.contains("ReflectorMap could not find class description")) {
+            Logger.getGlobal().info("post apply ignores unknown event " + body);
+         } else {
+            Logger.getGlobal().log(Level.SEVERE, "postApply failed", e);
+         }
+      }
+      return "apply done";
+   }
+
+   private void subscribeAndLoadOldEvents() {
+      ServiceSubscribed serviceSubscribed = new ServiceSubscribed()
+            .setServiceUrl(String.format("http://localhost:%d/apply", port));
+      String json = Yaml.encodeSimple(serviceSubscribed);
+      try {
+         String url = "http://localhost:42000/subscribe";
+         HttpResponse<String> response = Unirest.post(url).body(json).asString();
+         String body = response.getBody();
+         System.out.println("TestMikroShop subscribed to broker\n" + body);
+         YamlIdMap idMap = new YamlIdMap(Event.class.getPackageName());
+         idMap.decode(body);
+         Map<String, Object> objectMap = idMap.getObjIdMap();
+         for (Object obj : objectMap.values()) {
+            Event event = (Event) obj;
+            eventQueue.put(event);
+         }
+      } catch (Exception e) {
+         e.printStackTrace();
+      }
+   }
+
+   public Event waitForEvent(String id) {
+      while (true) {
+         Event e = history.get(id);
+
+         if (e != null) {
+            return e;
+         }
+
+         try {
+            e = eventQueue.poll(Configuration.timeout, TimeUnit.MILLISECONDS);
+         }
+         catch (Exception x) {
+            throw new RuntimeException(x);
+         }
+
+         if (e == null) {
+            throw new RuntimeException("event timeout");
+         }
+
+         System.out.println("Test got event " + e.getId());
+         history.put(e.getId(), e);
+      }
+   }
+
    @Before
    public void setTimeOut() {
       Configuration.timeout = Constants.TIME_OUT;
       Configuration.pageLoadTimeout = Configuration.timeout;
-      Configuration.browserPosition = "-3500x10"; // Constants.BROWSER_POS;
+      Configuration.browserPosition = "-1300x600"; // Constants.BROWSER_POS;
+      Configuration.browserSize="400x500";
       // Configuration.headless = Constants.HEADLESS;
    }
 
    @Test
-   public void MicroShop()
-   {
+   public void newMicroShopTest() {
+      Event e;
+      // start the event broker
+      eventBroker = new EventBroker();
+      eventBroker.start();
+
+      this.start();
+
+      // start service
+      WarehouseService warehouse = new WarehouseService();
+      warehouse.start();
+
+      // start service
+      MicroShopService microShop = new MicroShopService();
+      microShop.start();
+
+
+      // page 12:01
+      open("http://localhost:42001/page/12_01");
+
+      // page 12:02
+      open("http://localhost:42001/page/12_02");
+      $("#barcode").setValue("b001");
+      $("#type").setValue("red shoes");
+      $("#location").setValue("shelf 42");
+      $("#ok").click();
+
+      e = waitForEvent("12:02:01");
+      StoreCommand storeCommand = (StoreCommand) e;
+      assertThat(storeCommand.getBarcode()).isEqualTo("b001");
+      assertThat(storeCommand.getLocation()).isEqualTo("shelf 42");
+
+      System.out.println();
+
+   }
+
+   @Test
+   public void MicroShop() {
       // start the event broker
       eventBroker = new EventBroker();
       eventBroker.start();
@@ -438,35 +565,27 @@ public class TestMicroShop
       System.out.println();
    }
 
-   public void publish(Event event)
-   {
+   public void publish(Event event) {
       String yaml = Yaml.encodeSimple(event);
 
       try {
-         HttpResponse<String> response = Unirest.post("http://localhost:42000/publish")
-               .body(yaml)
-               .asString();
-               Thread.sleep(200);
-      }
-      catch (Exception e) {
+         HttpResponse<String> response = Unirest.post("http://localhost:42000/publish").body(yaml).asString();
+         Thread.sleep(200);
+      } catch (Exception e) {
          e.printStackTrace();
       }
    }
 
-   public boolean firePropertyChange(String propertyName, Object oldValue, Object newValue)
-   {
-      if (this.listeners != null)
-      {
+   public boolean firePropertyChange(String propertyName, Object oldValue, Object newValue) {
+      if (this.listeners != null) {
          this.listeners.firePropertyChange(propertyName, oldValue, newValue);
          return true;
       }
       return false;
    }
 
-   public PropertyChangeSupport listeners()
-   {
-      if (this.listeners == null)
-      {
+   public PropertyChangeSupport listeners() {
+      if (this.listeners == null) {
          this.listeners = new PropertyChangeSupport(this);
       }
       return this.listeners;
