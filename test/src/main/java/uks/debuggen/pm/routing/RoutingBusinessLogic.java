d5bd9d72c0a547925396125617cf510c1afb33a4
Albert <zuendorf@uni-kassel.de>
2021-10-15 16:51:52 +0200

    re adding routing lowercase

---
+++ test/src/main/java/uks/debuggen/pm/routing/RoutingBusinessLogic.java
@@ -0,0 +1,9 @@
+package uks.debuggen.pm.routing;
+
+public class RoutingBusinessLogic
+{
+
+    public void computeLength(Route route) {
+        route.setLength("175 km");
+    }
+}
--- test/src/test/java/uks/debuggen/pm/routing/TestRoutingBoard.java
+++ test/src/test/java/uks/debuggen/pm/routing/TestRoutingBoard.java
@@ -3,6 +3,7 @@ package uks.debuggen.pm.routing;
 import java.util.LinkedHashMap;
 import org.junit.Test;
 import static org.assertj.core.api.Assertions.assertThat;
+import org.fulib.FulibTools;
 
 public class TestRoutingBoard
 {
@@ -27,15 +28,23 @@ public class TestRoutingBoard
       leg2.setLength("71 km");
 
       Stop frankfurt = new Stop().setId("Frankfurt");
+
+      // create links
       route1.setStart(kassel);
       route1.setEnd(frankfurt);
       kassel.setDeparture(leg1);
       leg1.setTo(fulda);
       fulda.setDeparture(leg2);
       leg2.setTo(frankfurt);
+
+      FulibTools.objectDiagrams().dumpSVG("tmp/RoutingBoardStart.svg", route1, kassel, leg1, fulda, leg2, frankfurt);
+
       logic.computeLength(route1);
       assertThat(route1.getLength()).isEqualTo("175 km");
 
-      System.out.println("super");
+      FulibTools.objectDiagrams().dumpSVG("tmp/RoutingBoardEnd.svg", route1, kassel, leg1, fulda, leg2, frankfurt);
+
+
+      System.out.println();
    }
 }
