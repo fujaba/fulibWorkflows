a0b946910f6a1b21334954a673a2755d9caba2b3
Albert Zündorf <zuendorf@uni-kassel.de>
2021-11-11 14:29:53 +0100

    added html table dump example

--- test/src/test/java/uks/debuggen/pm/studyright/TestStudyRightBoard.java
+++ test/src/test/java/uks/debuggen/pm/studyright/TestStudyRightBoard.java
@@ -2,6 +2,7 @@ package uks.debuggen.pm.studyright;
 import java.util.LinkedHashMap;
 
 import org.fulib.FulibTools;
+import org.fulib.mockups.FulibMockups;
 import org.junit.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -13,8 +14,8 @@ public class TestStudyRightBoard
    {
       StudyGuideBusinessLogic logic = new StudyGuideBusinessLogic();
 
-      Student karli = new Student().setId("Karli");
-      karli.setMotivation(83);
+      Student carli = new Student().setId("carli");
+      carli.setMotivation(83);
 
       Stop stop1 = new Stop().setId("stop1");
       stop1.setMotivation(66);
@@ -35,20 +36,66 @@ public class TestStudyRightBoard
       r5.setCredits(29);
 
       // create links
-      karli.withStops(stop1);
+      carli.withStops(stop1);
       stop1.setRoom(r1);
       r1.withNeighbors(r2, r5);
       r2.withNeighbors(r1, r5);
       r4.withNeighbors(r5);
       r5.withNeighbors(r1, r2, r4);
 
-      FulibTools.objectDiagrams().dumpSVG("tmp/StudyRightBoardStart.svg", karli, stop1, r1, r2, r4, r5);
+      FulibTools.objectDiagrams().dumpSVG("tmp/StudyRightBoardStart.svg", carli, stop1, r1, r2, r4, r5);
 
-      logic.findRoute(karli);
-      assertThat(karli.getRoute()).isEqualTo("math -> calculus -> math -> modeling -> exam");
+      logic.findRoute(carli);
+      assertThat(carli.getRoute()).isEqualTo("math -> calculus -> math -> modeling -> exam");
 
-      FulibTools.objectDiagrams().dumpSVG("tmp/StudyRightBoardEnd.svg", karli, stop1, r1, r2, r4, r5);
+      FulibTools.objectDiagrams().dumpSVG("tmp/StudyRightBoardEnd.svg", carli, stop1, r1, r2, r4, r5);
 
+
+      System.err.println();
+   }
+
+   @Test
+   public void testStudyRightBoardTableDump()
+   {
+      StudyGuideBusinessLogic logic = new StudyGuideBusinessLogic();
+
+      Student carli = new Student().setId("carli");
+      carli.setMotivation(83);
+
+      Stop stop1 = new Stop().setId("stop1");
+      stop1.setMotivation(66);
+
+      Room r1 = new Room().setId("r1");
+      r1.setTopic("math");
+      r1.setCredits(17);
+
+      Room r2 = new Room().setId("r2");
+      r2.setTopic("calculus");
+      r2.setCredits(20);
+
+      Room r4 = new Room().setId("r4");
+      r4.setTopic("exam");
+
+      Room r5 = new Room().setId("r5");
+      r5.setTopic("modeling");
+      r5.setCredits(29);
+
+      // create links
+      carli.withStops(stop1);
+      stop1.setRoom(r1);
+      r1.withNeighbors(r2, r5);
+      r2.withNeighbors(r1, r5);
+      r4.withNeighbors(r5);
+      r5.withNeighbors(r1, r2, r4);
+
+      FulibTools.objectDiagrams().dumpSVG("tmp/StudyRightBoardStart.svg", carli, stop1, r1, r2, r4, r5);
+      FulibMockups.htmlTool().dumpTables("tmp/StudyRightBoardStart.html", carli, stop1, r1, r2, r4, r5);
+
+      logic.findRoute(carli);
+      assertThat(carli.getRoute()).isEqualTo("math -> calculus -> math -> modeling -> exam");
+
+      FulibTools.objectDiagrams().dumpSVG("tmp/StudyRightBoardEnd.svg", carli, stop1, r1, r2, r4, r5);
+      FulibMockups.htmlTool().dumpTables("tmp/StudyRightBoardEnd.html", carli, stop1, r1, r2, r4, r5);
 
       System.err.println();
    }
