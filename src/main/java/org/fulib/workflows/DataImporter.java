eae530d35897eff559a111b29cff130520326ba8
Maximilian Freiherr von Künßberg <maximilian-kuenssberg@t-online.de>
2021-11-19 18:59:31 +0100

    create classes, move to different pc

---
+++ src/main/java/org/fulib/workflows/DataImporter.java
@@ -0,0 +1,14 @@
+package org.fulib.workflows;
+
+import java.nio.file.Path;
+
+public class DataImporter {
+
+    public void importFromFile(Path filepath) {
+
+    }
+
+    public void importFromString(String filepath) {
+
+    }
+}
---
+++ src/main/java/org/fulib/workflows/HtmlGenerator.java
@@ -0,0 +1,4 @@
+package org.fulib.workflows;
+
+public class HtmlGenerator {
+}
