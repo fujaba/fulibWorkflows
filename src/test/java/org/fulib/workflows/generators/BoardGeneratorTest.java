db684e8a30586c5947137ca1cfd08aeb0f6f2765
Maximilian Freiherr von Künßberg <maximilian-kuenssberg@t-online.de>
2021-12-13 21:10:25 +0100

    generate fxmls similar to htmls

--- src/test/java/org/fulib/workflows/generators/BoardGeneratorTest.java
+++ src/test/java/org/fulib/workflows/generators/BoardGeneratorTest.java
@@ -35,7 +35,7 @@ public class BoardGeneratorTest {
         try {
             String yaml = Files.readString(Path.of("src/gen/resources/workflows.es.yaml"));
             Map<String, String> map = boardGenerator.generateAndReturnHTMLs(yaml);
-            Assert.assertEquals(5, map.size());
+            Assert.assertEquals(9, map.size()); // 1 Board, 4 Htmls, 4 Fxmls
         } catch (IOException e) {
             e.printStackTrace();
         }
