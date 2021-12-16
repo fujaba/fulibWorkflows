e139a26d088c1a67b641a3aaefa1cc2a54862439
Maximilian Freiherr von Künßberg <maximilian-kuenssberg@t-online.de>
2021-12-16 10:34:00 +0100

    add diagram generation part 1

--- src/main/java/org/fulib/workflows/generators/constructors/DiagramConstructor.java
+++ src/main/java/org/fulib/workflows/generators/constructors/DiagramConstructor.java
@@ -1,41 +1,81 @@
 package org.fulib.workflows.generators.constructors;
 
+import org.antlr.v4.runtime.misc.Pair;
 import org.fulib.FulibTools;
 import org.fulib.workflows.events.Data;
 import org.fulib.yaml.YamlIdMap;
-import org.fulib.yaml.YamlObject;
+import org.stringtemplate.v4.ST;
+import org.stringtemplate.v4.STGroupFile;
 
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.List;
+import java.util.Objects;
 
 public class DiagramConstructor {
+    private STGroupFile fulibYamlGroup;
+
     public String buildDiagram(List<Data> notes, int index) {
-        // TODO
-        YamlIdMap yamlIdMap = new YamlIdMap();
-        YamlObject yamlObject = new YamlObject();
+        String objectYaml = buildFulibYamlNotation(notes);
+
+        return generateDiagram(objectYaml, index);
+    }
+
+    private String buildFulibYamlNotation(List<Data> notes) {
+        fulibYamlGroup = new STGroupFile(Objects.requireNonNull(this.getClass().getResource("../FulibYaml.stg")));
+        StringBuilder yamlBody = new StringBuilder();
 
         for (Data note : notes) {
-            for (Integer integer : note.getData().keySet()) {
-                yamlObject.with(note.getName(), note.getData().get(integer).a);
-                yamlObject.with(note.getName(), note.getData().get(integer).b);
-            }
+            ST st = fulibYamlGroup.getInstanceOf("object");
+
+            String[] s = note.getName().split(" ");
+
+            st.add("name", s[1]);
+            st.add("type", s[0]);
+            st.add("attributes", buildAttributes(note));
+
+            yamlBody.append(st.render());
         }
 
-        String test = yamlIdMap.encode(yamlObject);
+        return yamlBody.toString();
+    }
 
-        String fileName = FulibTools.objectDiagrams().dumpSVG("tmp/test/diagram_" + index, yamlObject);
+    private String buildAttributes(Data note) {
+        StringBuilder attributesBody = new StringBuilder();
 
-        String result = null;
+        for (int i = 0; i < note.getData().size(); i++) {
+            Pair<String, String> pair = note.getData().get(i);
+            ST st = fulibYamlGroup.getInstanceOf("attribute");
+
+            st.add("type", pair.a);
+            st.add("value", pair.b);
+
+            attributesBody.append(st.render());
+        }
+
+        return attributesBody.toString();
+    }
+
+    private String generateDiagram(String objectYaml, int index) {
+        String fileName = "tmp/test/diagram_" + index;
+        String result = "";
+
+        YamlIdMap idMap = new YamlIdMap();
+        Object root = idMap.decode(objectYaml);
+
+        fileName = FulibTools.objectDiagrams().dumpSVG(fileName, root);
 
         try {
-            result = Files.readString(Path.of(fileName));
+            result = Files.readString(Path.of(fileName + ".svg"));
 
-            Files.deleteIfExists(Path.of(fileName));
+            Files.deleteIfExists(Path.of(fileName + ".svg"));
+
+            Files.deleteIfExists(Path.of("tmp/test/"));
         } catch (IOException e) {
             e.printStackTrace();
         }
+
         return result;
     }
 }
